fa52537388b50cd422278ae95a57b4145d1cf607
黄熠皓 <141047642+XyIitoI@users.noreply.github.com>
2023-09-18 19:57:16 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,8 +52,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'Home',
-      link: 'https://home.xylitol.cc',
+      name: 'User',
+      link: 'https://onedrivemanager.sucralose.cc',
     },
   ],
 
