1df01c03c3b3caf768b7b2fb5a2d16dd2ccb7465
黄熠皓 <141047642+XyIitoI@users.noreply.github.com>
2023-09-19 11:47:41 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'user',
+      name: 'Github',
       link: 'https://onedrivemanager.sucralose.cc/?login=admin',
     },
   ],
