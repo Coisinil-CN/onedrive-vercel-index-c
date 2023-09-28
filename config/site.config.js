5b2e57a10dcdb5111d23a3dce53778bfd88c287f
黄熠皓 <141047642+XyIitoI@users.noreply.github.com>
2023-09-28 19:11:38 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Vercel/计算机', '/Vercel/理学'],
+  protectedRoutes: ["/Vercel/计算机", '/Vercel/理学'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "",
