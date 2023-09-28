063ac226c7b5febfd0f2a9a44ea8c291b91d5976
黄熠皓 <141047642+XyIitoI@users.noreply.github.com>
2023-09-28 22:12:15 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'Github',
-      link: 'https://onedrivemanager.sucralose.cc/?login=admin',
+      link: 'https://https://github.com/XyIitoI/',
     },
   ],
 
